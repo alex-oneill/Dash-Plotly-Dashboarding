13d726e0d20d30d3bb52db0ecf226dd313ad148d
Alex ONeill <73564404+alex-oneill@users.noreply.github.com>
2021-04-24 13:37:42 -0400

    completed project

--- main_app.py
+++ main_app.py
@@ -1,17 +1,22 @@
-import dash_table
+"""
+Author: ALEX ONEILL
+CS632P - Python Programming (Prof. Sarbanes)
+Project 2 - Visualizing Stock Data with Dash/Plotly
+"""
+
 import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table as dt
 import plotly.express as px
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import base64
 import io
-import numpy as np
 
-app = dash.Dash()
+
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
 # SECTION: CARDS
 # NOTE: HEADER
@@ -24,7 +29,7 @@ header = dbc.Jumbotron(
             ], fluid=True, id='jumbotron_header', className='jumbotron', style={'paddingTop': '1rem',
                                                                                 'paddingBottom': '1rem'})
 
-# NOTE: FILE BROWSER
+# NOTE: FILE BROWSER CARD
 file_browser = dbc.Card([
                     dbc.CardHeader('Browser', className='card text-white bg-primary mb-3'),
                     dbc.CardBody([
@@ -40,13 +45,17 @@ file_browser = dbc.Card([
                     dbc.CardFooter(id='card_footer')
                 ], className='card text-white bg-dark mb-3', id='card_browser')
 
-# NOTE: DATATYPE PICKER
+# NOTE: DATATYPE PICKER CARD
 data_picker = dbc.Card([
                     dbc.CardHeader('Select Columns', className='card text-white bg-primary mb-3'),
-                    dbc.CardBody([], id='card-table-body')
+                    dbc.CardBody(id='data_picker_card_body',
+                                 children=dbc.Table(bordered=True, className='table-secondary',
+                                                    id='data_picker_table')),
+                    dbc.CardFooter(id='picker_footer', style={'textAlign': 'center', 'verticalAlign': 'middle'}),
+                    dbc.CardFooter(id='picker_footer_2', style={'textAlign': 'center', 'verticalAlign': 'middle'})
                 ], className='card text-white bg-dark mb-3', id='card_dataPicker')
 
-# NOTE: DATA PREVIEW TABLE
+# NOTE: DATA PREVIEW TABLE CARD
 dropdown_style = {'color': '#55595c', 'fontFamily': 'monospace', 'textTransform': 'uppercase'}
 preview = dbc.Card([
                     dbc.CardHeader('Data Preview', className='card text-white bg-primary mb-3'),
@@ -61,25 +70,26 @@ preview = dbc.Card([
                                                  'textAlign': 'left'},
                                      style_table={'height': 300, 'overflowY': 'auto'},
                                      page_size=50)
-
-                    ])
+                    ]),
+                    dbc.CardFooter(id='preview_footer', style={'textAlign': 'center', 'verticalAlign': 'middle'})
                 ], className='card text-white bg-dark mb-3', id='card_dataPreview')
 
-# NOTE: DISPLAY GRAPH
-x = np.arange(10)
+# NOTE: DISPLAY GRAPH CARD
 chart = dbc.Card([
-                    dbc.CardHeader('Chart'),
+                    dbc.CardHeader('Chart', className='card text-white bg-primary mb-3'),
                     dbc.CardBody([
-                        dcc.Dropdown(disabled=True, style=dropdown_style, placeholder='Select a feature'),
+                        dcc.Dropdown(disabled=True, style=dropdown_style, placeholder='Select a feature',
+                                     id='chart_dd'),
                         html.Br(),
-                        dcc.Dropdown(disabled=True, style=dropdown_style, placeholder='Select a security'),
-                        html.Div('chart'),
+                        dcc.Dropdown(disabled=True, style=dropdown_style, placeholder='Select a security',
+                                     id='chart_dd2', multi=True),
+                        html.Br(),
                         dcc.Graph(id='px_figure')
                     ])
                 ], className='card text-white bg-dark mb-3', id='card_chart')
 
-
 # SECTION: APP LAYOUT
+tbl_styles = {'verticalAlign': 'middle', 'textAlign': 'center'}
 app.layout = dbc.Container([
         dbc.Row([
             dbc.Col(header)
@@ -90,14 +100,18 @@ app.layout = dbc.Container([
                 data_picker], width=4),
             dbc.Col([
                 html.Div(id='hidden_df', style={'display': 'none'}),
-                html.Div(id='hidden_fdf', style={'display': 'none'}),
+                html.Div(id='hidden_fdf_1', style={'display': 'none'}),
+                html.Div(id='hidden_fdf_final', style={'display': 'none'}),
+                html.Div(id='printer_list', style={'display': 'none'}, children=[]),
+                html.Div(id='df_config', style={'display': 'none'}),
                 preview,
                 chart], width=8)
         ])
 ], fluid=True)
 
 
-# SECTION: UPLOAD SUCCESSFUL PROMPT & RESET BUTTON
+# SECTION: CALLBACKS
+# NOTE: 1--UPLOAD SUCCESSFUL // RESET BUTTON
 @app.callback([Output('card_footer', 'children'),
                Output('upload', 'disabled')],
               Input('upload', 'filename'))
@@ -114,7 +128,7 @@ def show_filename(file_name):
         return upload_complete, True
 
 
-# SECTION: STORE FULL DF
+# NOTE: 2--STORE UPLOADED DF
 @app.callback(Output('hidden_df', 'children'),
               Input('upload', 'contents'))
 def parse_full_file(contents):
@@ -127,68 +141,150 @@ def parse_full_file(contents):
         return df.to_json(orient='split')
 
 
-# SECTION: PARSING UPLOAD DATA
-@app.callback([Output('hidden_fdf', 'children'),
-              Output('card-table-body', 'children')],
-              Input('upload', 'contents'))
-def parse_file(contents):
-    if not contents:
-        raise dash.exceptions.PreventUpdate
-    else:
-        content_type, content_string = contents.split(',')
-        decoded = base64.b64decode(content_string)
-        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-        cols = df.columns.tolist()
-        dtypes = ['object/string', 'int64', 'float64', 'bool', 'datetime64']
-        tbl_styles = {'verticalAlign': 'middle', 'textAlign': 'center'}
-        headers = [
-            html.Thead(html.Tr([html.Th('Column', style=tbl_styles), html.Th('Type', style=tbl_styles),
-                                html.Th('Ignore', style=tbl_styles)]))]
-        id = 0
-        for col in cols:
-            dropdown = dbc.Select(id='dtype_dropdown_'+str(id), placeholder='Select a dtype',
-                                  options=[{'label': type, 'value': type} for type in dtypes],
+# NOTE: 3--MAKE PICKER TABLE
+@app.callback([Output('data_picker_table', 'children'),
+               Output('picker_footer', 'children')],
+              Input('hidden_df', 'children'))
+def make_picker_table(hidden_df):
+    df = pd.read_json(hidden_df, orient='split')
+    cols = df.columns.tolist()
+    headers = [
+        html.Thead(html.Tr([html.Th('Column', style=tbl_styles), html.Th('Casted Type', style=tbl_styles),
+                            html.Th('Ignore', style=tbl_styles)]))]
+    id = 0
+    for col in cols:
+        if df[col].dtype == 'datetime64[ns]':
+            dropdown = dbc.Select(id='dtype_dropdown_' + str(id), placeholder='Casted dtype',
+                                  options=[{'label': 'datetime64', 'value': 'datetime64'},
+                                           {'label': 'object/string', 'value': 'object'}],
                                   className='custom-select')
-            toggle = dbc.Checklist(id='col_toggle_'+str(id), options=[{"label": "Drop", "value": 1}],
-                                   value=[], switch=True)
-            headers.append(
-                html.Tr(id='data_row_'+str(id), children=[html.Td(col, style=tbl_styles),
-                                                          html.Td(dropdown, style=tbl_styles),
-                                                          html.Td(toggle, style=tbl_styles)]))
-            id += 1
-        table = dbc.Table(headers, striped=True, bordered=True, className='table-secondary')
-        return df.to_json(orient='split'), table
+        elif df[col].dtype == 'int64' or df[col].dtype == 'float64':
+            dropdown = dbc.Select(id='dtype_dropdown_'+str(id), placeholder='Casted dtype',
+                                  options=[{'label': 'int64', 'value': 'int64'},
+                                           {'label': 'float64', 'value': 'float64'},
+                                           {'label': 'object/string', 'value': 'object'}],
+                                  className='custom-select')
+        elif df[col].dtype == 'object':
+            dropdown = dbc.Select(id='dtype_dropdown_'+str(id), placeholder='Casted dtype',
+                                  options=[{'label': 'object/string', 'value': 'object'}],
+                                  className='custom-select')
+        toggle = dbc.Checklist(id='column_toggle_' + str(id),
+                               options=[{'label': 'Drop', 'value': 1}],
+                               switch=True)
+        headers.append(
+            html.Tr(id='data_row_'+str(id),
+                    children=[html.Td(col, style=tbl_styles, id='col_name_'+str(id)),
+                              html.Td(dropdown, style=tbl_styles),
+                              html.Td(toggle, style=tbl_styles)]))
+        id += 1
+    drop_cols = dbc.Button('Confirm Dropped Rows', className='btn btn-warning', id='confirm_dropped_rows',
+                           style={'width': '50%'})
+    return headers, drop_cols
 
 
-# SECTION: BUILD DYNAMIC TABLE
-@app.callback([Output('preview_table', 'data'),
-              Output('preview_table', 'columns')],
-              Input('hidden_df', 'children'))
-def make_preview_table(hidden_json_data):
-    new_data = pd.read_json(hidden_json_data, orient='split')
-    data = new_data.to_dict('records')
-    column_headers = [{'name': key, 'id': key} for key in data[0].keys()]
-    return data, column_headers
+# NOTE: 4--DROP COLUMNS FOR PREVIEW
+@app.callback([Output('hidden_fdf_1', 'children'),
+               Output('picker_footer_2', 'children'),
+               Output('confirm_dropped_rows', 'disabled')],
+              Input('confirm_dropped_rows', 'n_clicks'),
+              [State('hidden_df', 'children')] + [(State('column_toggle_'+str(num), 'value')) for num in range(5)])
+def drop_cols(n_clicks, hidden_df, col0, col1, col2, col3, col4):
+    if not n_clicks:
+        raise dash.exceptions.PreventUpdate()
+    elif n_clicks > 0:
+        cols = [col0, col1, col2, col3, col4]
+        temp_data = pd.read_json(hidden_df, orient='split')
+        col_names = temp_data.columns.tolist()
+        for i, col in enumerate(cols):
+            if col:
+                temp_data.drop(columns=col_names[i], inplace=True)
+        cast_cols = dbc.Button('Confirm Casted Types', className='btn btn-warning', id='confirm_casts',
+                               style={'width': '50%'})
+        return temp_data.to_json(orient='split'), cast_cols, True
 
 
-# SECTION: BUILD PREVIEW TABLE DROPDOWN
+# NOTE: 5--CAST COLUMNS FOR PREVIEW
+@app.callback([Output('hidden_fdf_final', 'children'),
+               Output('confirm_casts', 'disabled')],
+              Input('confirm_casts', 'n_clicks'),
+              [State('hidden_fdf_1', 'children')] + [(State('dtype_dropdown_'+str(num), 'value')) for num in range(5)] +
+              [State('hidden_df', 'children')])
+def cast_cols(n_clicks, hidden_df, col0, col1, col2, col3, col4, orig_df):
+    if not n_clicks:
+        raise dash.exceptions.PreventUpdate()
+    elif n_clicks > 0:
+        cols = [col0, col1, col2, col3, col4]
+        temp_data = pd.read_json(hidden_df, orient='split')
+        orig = pd.read_json(orig_df, orient='split')
+        orig_col_names = orig.columns.tolist()
+        for i, dtype in enumerate(cols):
+            if dtype:
+                col_name = orig_col_names[i]
+                temp_data[col_name] = temp_data[col_name].astype(dtype)
+        return temp_data.to_json(orient='split'), True
+
+
+# NOTE: 6--PREVIEW TABLE DROPDOWN
 @app.callback(Output('table_filter_dropdown', 'options'),
-              Input('hidden_df', 'children'))
+              Input('hidden_fdf_final', 'children'))
 def make_preview_table_dropdown(hidden_json_data):
     new_data = pd.read_json(hidden_json_data, orient='split')
     drop_tickers = [{'label': ticker, 'value': ticker} for ticker in new_data['Stock'].unique().tolist()]
     return drop_tickers
 
 
-# SECTION: BUILD CHART
+# NOTE: 7--PREVIEW TABLE
+@app.callback([Output('preview_table', 'data'),
+               Output('preview_table', 'columns'),
+               Output('preview_footer', 'children')],
+              Input('table_filter_dropdown', 'value'),
+              State('hidden_fdf_final', 'children'))
+def make_preview_table(securities, hidden_json_data):
+    if not hidden_json_data:
+        raise dash.exceptions.PreventUpdate
+    else:
+        new_data = pd.read_json(hidden_json_data, orient='split')
+        data = new_data.to_dict('records')
+        column_headers = [{'name': key, 'id': key} for key in data[0].keys()]
+        fdf = new_data.loc[new_data['Stock'].isin(securities)]
+        data = fdf.to_dict('records')
+        chart_button = dbc.Button('Activate Charting Options', className='btn btn-success', id='build_chart',
+                                  style={'width': '50%'})
+        return data, column_headers, chart_button
+
+
+# NOTE: 8--ACTIVATE CHART DROPDOWNS
+@app.callback([Output('chart_dd', 'options'),
+               Output('chart_dd', 'disabled'),
+               Output('chart_dd2', 'options'),
+               Output('chart_dd2', 'disabled'),
+               Output('build_chart', 'disabled')],
+              Input('build_chart', 'n_clicks'),
+              State('hidden_df', 'children'))
+def make_chart(click, hidden_json_data):
+    if not hidden_json_data or not click:
+        raise dash.exceptions.PreventUpdate
+    elif click > 0:
+        new_data = pd.read_json(hidden_json_data, orient='split')
+        dd1_options = [{'label': col, 'value': col} for col in ['Volume', 'Adj Close']]
+        dd2_options = [{'label': ticker, 'value': ticker} for ticker in new_data['Stock'].unique().tolist()]
+        return dd1_options, False, dd2_options, False, True
+
+
+# NOTE: 9--FILTERING CHART DISPLAY
 @app.callback(Output('px_figure', 'figure'),
-              Input('hidden_df', 'children'))
-def make_chart(df):
-    new_data = pd.read_json(df, orient='split')
-    # close_price = new_data['Adj Close'].to_list()
-    # dates = new_data['Date'].to_list()
-    out_data = px.line(new_data, x='Date', y='Adj Close', color='Stock')
-    return out_data
+              [Input('chart_dd', 'value'),
+               Input('chart_dd2', 'value')],
+              State('hidden_df', 'children'))
+def filter_chart(feature, securities, data):
+    if not feature or not securities or not data:
+        raise dash.exceptions.PreventUpdate
+    else:
+        df = pd.read_json(data, orient='split')
+        fdf = df.loc[df['Stock'].isin(securities)]
+        data = fdf.to_dict('records')
+        fig_out = px.line(data, x='Date', y=feature, color='Stock')
+        return fig_out
 
 
 if __name__ == '__main__':
